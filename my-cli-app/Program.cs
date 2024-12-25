f721fddba29a44dd4330816c211be36ff1829886
mickey-octodemo2 <93947918+mickey-octodemo2@users.noreply.github.com>
2024-12-25 23:41:22 +0000

    feat: Test my app for division by zero. Currently this is buggy due to bad package

--- my-cli-app/Program.cs
+++ my-cli-app/Program.cs
@@ -10,8 +10,8 @@ namespace MyCliApp
         var addResult = Calculator.Add(2, 3);
         Console.WriteLine($"2 + 3 = {addResult}");
 
-        var divideResult = Calculator.Divide(10, 2);
-        Console.WriteLine($"10 / 2 = {divideResult}");
+        var divideResult = Calculator.Divide(10, 0);
+        Console.WriteLine($"10 / 0 = {divideResult}");
         }
     }
 }
